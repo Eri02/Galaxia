6b289e4185bfa39318b1c5f1cfb60c30e55299de
Erika Roque <erikaroque2@gmail.com>
2019-10-11 22:55:30 -0300

    fórmula de herón

--- src/domain/SistemaSolar.java
+++ src/domain/SistemaSolar.java
@@ -5,12 +5,6 @@ import java.awt.geom.Point2D.Double;
 
 public class SistemaSolar {
 	
-	double pendiente;
-	double independiente;
-	
-	double x;
-	double y;
-	
 	public void getClimaPorDia() {
 		
 	}
@@ -40,18 +34,36 @@ public class SistemaSolar {
 		//return this.formanUnTriangulo(posP1, posP2, posP3);
 	}
 
-	private void solPerteneceAlTriangulo(Point2D posP1, Point2D posP2, Point2D posP3, Point2D sol) {
-		// TODO Auto-generated method stub
+	public boolean solPerteneceAlTriangulo(Point2D p1, Point2D p2, Point2D p3, Point2D sol) {
+		double l1 = this.distanciaEntreDosPuntos(p1, p2);
+		double l2 = this.distanciaEntreDosPuntos(p2, p3);
+		double l3 = this.distanciaEntreDosPuntos(p3, p1);
 		
+		double area = this.areaDeTriangulo(l1, l2, l3);
+		
+		double pb = this.distanciaEntreDosPuntos(sol, p2);
+		double pc = this.distanciaEntreDosPuntos(sol, p3);
+		double pa = this.distanciaEntreDosPuntos(sol, p1);
+		
+		double a = (pb * pc)/(2 * area);
+		double b = (pc * pa)/(2 * area);
+		double c = 1 - a - b;
+		return (0 <= a && a <= 1) && (0 <= b && b <= 1) && (0 <= c && c <= 1);
+	}
+	
+	//Fórmula de Herón
+	public double areaDeTriangulo(double a, double b, double c) {
+		double s = (a + b + c)/2;
+		double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+		return area;
 	}
 
 	public boolean formanUnTriangulo(Point2D p1, Point2D p2, Point2D p3){
-
 		double a = this.distanciaEntreDosPuntos(p1, p2);
 		double b = this.distanciaEntreDosPuntos(p2, p3);
 		double c = this.distanciaEntreDosPuntos(p3, p1);
 
-		return (a >=1 && b >= 1 && c >= 1);
+		return (a >=1 && b >= 1 && c >= 1); //revisar
 	}
 
 	public double perimetro(Point2D p1, Point2D p2, Point2D p3) {
@@ -65,8 +77,8 @@ public class SistemaSolar {
 	//a, b , c distancia entre los puntos (posici�n) de C/planeta
 	public double distanciaEntreDosPuntos(Point2D p1, Point2D p2) {
 		//d= raiz[(x2-x1)2+(y2-y1)2]
-		double x = p2.getX() - p1.getX();
-		double y = p2.getY() - p1.getY();
+		double x = Math.abs(p2.getX() - p1.getX());
+		double y = Math.abs(p2.getY() - p1.getY());
 		double d = Math.sqrt((Math.pow(x, 2) + (Math.pow(y, 2))));		
 		return d;
 	}
