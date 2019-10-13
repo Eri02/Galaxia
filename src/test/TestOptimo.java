3e935cade579c64a50c40720729332ae2c7308c2
Erika Roque <erikaroque2@gmail.com>
2019-10-13 14:15:27 -0300

    test sequia

---
+++ src/test/TestOptimo.java
@@ -0,0 +1,25 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.geom.Point2D;
+
+import org.junit.jupiter.api.Test;
+
+import domain.Clima;
+import domain.Optimo;
+import junit.framework.TestCase;
+
+class TestOptimo extends TestCase{
+	
+	private Optimo optimo = new Optimo();
+	Point2D p1 = new Point2D.Double(4.0,2.0);
+	Point2D p2 = new Point2D.Double(4.0,4.0);
+	Point2D p3 = new Point2D.Double(4.0,6.0);
+
+	@Test
+	void testEsOptimo() {
+		assertEquals(true, optimo.esOptimo(p1, p2, p3));
+	}
+
+}
--- src/test/TestSequia.java
+++ src/test/TestSequia.java
@@ -2,13 +2,26 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
+
 import org.junit.jupiter.api.Test;
 
-class TestSequia {
+import domain.Optimo;
+import domain.Sequia;
+import junit.framework.TestCase;
 
+class TestSequia extends TestCase {
+
+	private Sequia sequia = new Sequia();
+	Point2D p1 = new Point2D.Double(4.0,2.0);
+	Point2D p2 = new Point2D.Double(4.0,4.0);
+	Point2D p3 = new Point2D.Double(4.0,6.0); 
+	Point2D sol = new Double(0, 0);
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testHaySequia() {
+		assertEquals(true, sequia.haySequia(p1, p2, p3, sol));
 	}
 
 }
