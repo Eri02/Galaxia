f7ddc17ce0d9846bd471583b0af52ee4e2271181
Erika Roque <erikaroque2@gmail.com>
2019-10-13 19:58:57 -0300

    info periodo

---
+++ src/domain/InfoPeriodo.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class InfoPeriodo {
+
+	private String clima;
+	private int periodo;
+	
+	public InfoPeriodo() {
+		
+	} 
+	
+	public InfoPeriodo(String tipo, int periodo) {
+		this.clima = tipo;
+		this.periodo = periodo;
+	}
+	
+	public void setClima(String clima) {
+		this.clima = clima;
+	}
+	
+	public void setPeriodo(int periodo) {
+		this.periodo = periodo;
+	}
+	
+	public String getClima() {
+		return this.clima;
+	}
+	
+	public int getPeriodo() {
+		return this.periodo;
+	}
+}
